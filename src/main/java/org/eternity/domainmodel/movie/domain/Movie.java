8a831da720c262428c4da3c135a2bdb14ce6e2ef
조영호 <aeternum9342@gmail.com>
2025-01-21 09:58:03 +0900

    프로젝트 초기화

---
+++ src/main/java/org/eternity/domainmodel/movie/domain/Movie.java
@@ -0,0 +1,42 @@
+package org.eternity.domainmodel.movie.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.eternity.domainmodel.generic.Money;
+
+import java.util.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Movie {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Integer runningTime;
+    private Money fee;
+
+    @ElementCollection
+    @CollectionTable(name = "PRICES", joinColumns = @JoinColumn(name="MOVIE_ID"))
+    private Set<Money> prices = new HashSet<>();
+
+//    @OneToMany(cascade = CascadeType.PERSIST)
+//    @JoinColumn(name="MOVIE_ID")
+//    private Collection<Screening> screenings = new ArrayList<>();
+
+    public Movie(String title, Integer runningTime, Money fee) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void addPrice(Money price) {
+        this.prices.add(price);
+    }
+}
---
+++ src/main/resources/a.sql
@@ -0,0 +1,1 @@
+delete from screening where id=2
