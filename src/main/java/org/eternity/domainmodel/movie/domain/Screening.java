319c79ebb7aa87543ffe5323219186bc94f8ce0c
조영호 <aeternum9342@gmail.com>
2025-01-21 09:58:03 +0900

    프로젝트 초기화

---
+++ src/main/java/org/eternity/domainmodel/movie/domain/Screening.java
@@ -0,0 +1,35 @@
+package org.eternity.domainmodel.movie.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Screening {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int sequence;
+    private LocalDateTime screeningTime;
+
+    public Screening(int sequence, LocalDateTime screeningTime) {
+        this.sequence = sequence;
+        this.screeningTime = screeningTime;
+    }
+
+//    @ManyToOne
+//    @JoinColumn(name="MOVIE_ID")
+//    private Movie movie;
+//
+//    public Screening(Movie movie, int sequence, LocalDateTime screeningTime) {
+//        this.movie = movie;
+//        this.movie.addScreening(this);
+//        this.sequence = sequence;
+//        this.screeningTime = screeningTime;
+//    }
+}
---
+++ src/test/java/org/eternity/domainmodel/movie/domain/JpaCollectionTest.java
@@ -0,0 +1,46 @@
+package org.eternity.domainmodel.movie.domain;
+
+import jakarta.persistence.EntityManager;
+import org.eternity.domainmodel.generic.Money;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+
+@DataJpaTest(showSql = false)
+public class JpaCollectionTest {
+	@Autowired
+	private EntityManager em;
+
+	@Test
+	public void persist() {
+		Movie movie = new Movie("한산", 120, Money.wons(10000));
+		movie.addPrice(Money.wons(2000));
+
+	    em.persist(movie);
+		em.flush();
+		em.clear();
+
+		Movie loadedMovie = em.find(Movie.class, movie.getId());
+		loadedMovie.addPrice(Money.wons(1000));
+
+		em.flush();
+	}
+
+		@Test
+	public void remove() {
+		Movie movie = new Movie("한산", 120, Money.wons(10000));
+		movie.addPrice(Money.wons(1000));
+		movie.addPrice(Money.wons(2000));
+
+		em.persist(movie);
+		em.flush();
+		em.clear();
+
+		Movie loadedMovie = em.find(Movie.class, movie.getId());
+		loadedMovie.getPrices().clear();
+
+		em.flush();
+	}
+}
